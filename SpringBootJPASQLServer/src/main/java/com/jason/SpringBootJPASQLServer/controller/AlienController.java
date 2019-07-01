d41bff49cc4a1d4afb7e4dbc7b18eb02b26106ad
norazhaoo <haoyu039@gmail.com>
2019-07-01 11:46:37 -0400

    Json format

--- SpringBootJPASQLServer/src/main/java/com/jason/SpringBootJPASQLServer/controller/AlienController.java
+++ SpringBootJPASQLServer/src/main/java/com/jason/SpringBootJPASQLServer/controller/AlienController.java
@@ -1,9 +1,14 @@
 package com.jason.SpringBootJPASQLServer.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jason.SpringBootJPASQLServer.dao.AlienRepo;
@@ -27,11 +32,19 @@ public class AlienController {
 		return "home.jsp";
 	}
 
-	@RequestMapping("/getAlien")
-	public ModelAndView getAlien(@RequestParam int aid) {
-		ModelAndView mv = new ModelAndView("showAlien.jsp");
-		Alien alien = repo.findById(aid).orElse(new Alien());
-		mv.addObject(alien);
-		return mv;
+	@RequestMapping("/aliens")
+	@ResponseBody
+	public List<Alien> getAliens() {
+		
+		return repo.findAll();
+
+	}
+	
+	@RequestMapping("/aliens/{aid}")
+	@ResponseBody
+	public Optional<Alien> getAliens(@PathVariable("aid")int aid){
+		
+		return repo.findById(aid);
+
 	}
 }
--- SpringBootJPASQLServer/src/main/java/com/jason/SpringBootJPASQLServer/dao/AlienRepo.java
+++ SpringBootJPASQLServer/src/main/java/com/jason/SpringBootJPASQLServer/dao/AlienRepo.java
@@ -1,9 +1,18 @@
 package com.jason.SpringBootJPASQLServer.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jason.SpringBootJPASQLServer.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer>{
+import java.util.List;
+
+public interface AlienRepo extends JpaRepository<Alien, Integer>{
 	
+	List<Alien> findByTech(String tsech);
+	
+	List<Alien> findByAidGreaterThan(int aid);
+	
+	@Query("from Alien where tech=?1 order by aname")
+	List<Alien> findByTechSorted(String tech);
 }
