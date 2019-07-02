b076607a504fa59c75b3c9872f9942f74eeab3bf
Jason <jasongenekwak@gmail.com>
2019-07-02 11:53:49 -0400

    added Haoyu's code

--- MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/controller/ProjectController.java
+++ MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/controller/ProjectController.java
@@ -1,9 +1,15 @@
 package com.mmd.MMDSpringboot.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mmd.MMDSpringboot.dao.ResourceDao;
@@ -14,11 +20,30 @@ import com.mmd.MMDSpringboot.model.Resource;
 public class ProjectController {
 	
 	@Autowired
-	ResourceDao resourceDao;
+    ResourceDao resourceDao;
+
+    @GetMapping("/resource")
+    public List<Resource> getResources() {
+        return resourceDao.findAll();
+    }
+
+    @RequestMapping("/resource/{id}")
+    public Optional<Resource> getResource(@PathVariable("id") int id) {
+
+        return resourceDao.findById(id);
+
+    }
+    
+    @PostMapping("/resource")
+    public Resource addResource(Resource resource) {
+        return resourceDao.save(resource);
+    }
+    
+    @PutMapping(path = "/resource", consumes = {"application/json"})
+    public Resource saveOrUpdateResource(@RequestBody Resource resource) {
+        resourceDao.save(resource);
+        return resource;
+    }
 	
-	@GetMapping(path="/resource")
-	public List<Resource> getResources(){
-		return resourceDao.findAll();
-	}
 
 }
--- MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/dao/ResourceDao.java
+++ MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/dao/ResourceDao.java
@@ -1,6 +1,5 @@
 package com.mmd.MMDSpringboot.dao;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
