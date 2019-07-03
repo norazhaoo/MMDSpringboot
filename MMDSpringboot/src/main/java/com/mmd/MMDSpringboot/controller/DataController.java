d044d12b868f090fb4fb4823308c00f7b4a6f66e
jakwakcoder <jasongenekwak@gmail.com>
2019-07-03 15:30:04 -0400

    pausing for chen

--- MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/controller/DataController.java
+++ MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/controller/DataController.java
@@ -1,8 +1,15 @@
 package com.mmd.MMDSpringboot.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mmd.MMDSpringboot.dao.DataDao;
@@ -14,11 +21,21 @@ public class DataController {
 	@Autowired
 	DataDao dataDao;
 	
+    @RequestMapping("/data/{id}")
+    public List<Data> getData(@PathVariable("projectid") int id) {
+        return dataDao.findAllByProjectid(id);
+    }
+	
 	@PostMapping("/addData")
 	public Data addData(@RequestBody Data data) {
 		return dataDao.save(data);
-		
 	}
+	
+    @PutMapping(path = "/updateData", consumes = {"application/json"})
+    public Data saveOrUpdateData(@RequestBody Data data) {
+    	dataDao.save(data);
+        return data;
+    }
 	
 	
 }
--- MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/dao/DataDao.java
+++ MMDSpringboot/src/main/java/com/mmd/MMDSpringboot/dao/DataDao.java
@@ -1,9 +1,13 @@
 package com.mmd.MMDSpringboot.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mmd.MMDSpringboot.model.Data;
 
 public interface DataDao extends JpaRepository<Data, Integer>{
+
+	List<Data> findAllByProjectid(int id);
 
 }
